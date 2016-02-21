f36ae3a8e2eecc550af1f8f49209158c80c16ea4
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-21 11:38:46 +0100

    Copyright year update

--- doc/conf.py
+++ doc/conf.py
@@ -75,7 +75,7 @@ master_doc = 'index'
 # General information about the project.
 authors = 'Matthias Geier'
 project = 'python-sounddevice'
-copyright = '2015, ' + authors
+copyright = '2016, ' + authors
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
