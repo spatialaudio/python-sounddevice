9af4e43a387e312e1c79e35eacb65b138b816c83
Matthias Geier <Matthias.Geier@gmail.com>
2017-08-19 17:15:16 +0200

    For .active property, check for .closed first
    This way, a closed stream can be active==False instead of throwing an
    exception.

--- sounddevice.py
+++ sounddevice.py
@@ -1162,6 +1162,8 @@ class _StreamBase(object):
         stopped
 
         """
+        if self.closed:
+            return False
         return _check(_lib.Pa_IsStreamActive(self._ptr)) == 1
 
     @property
