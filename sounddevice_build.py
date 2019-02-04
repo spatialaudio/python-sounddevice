694dc07f30351e7b2cca9acf6bd400fecfdeee07
Pierre-Yves Taunay <5438710+pytaunay@users.noreply.github.com>
2019-02-04 13:02:34 -0500

    Remove C file descriptor backend that are unecessary

--- sounddevice_build.py
+++ sounddevice_build.py
@@ -309,13 +309,5 @@ typedef struct PaWasapiStreamInfo
 } PaWasapiStreamInfo;
 """)
 
-ffibuilder.cdef("""
-    /* from stdio.h */
-    FILE* fopen(const char* path, const char* mode);
-    int fclose(FILE* fp);
-    FILE* stderr;  /* GNU C library */
-    FILE* __stderrp;  /* macOS */
-""")
-
 if __name__ == "__main__":
     ffibuilder.compile(verbose=True)
