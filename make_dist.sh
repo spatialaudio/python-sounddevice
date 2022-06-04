f4607b357e5dd1dddb1858dafdf8e50606f55907
Matthias Geier <Matthias.Geier@gmail.com>
2022-06-04 20:11:53 +0200

    Update build script to use "build"

--- make_dist.sh
+++ make_dist.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
+set -euo pipefail
+
 # Create a source distribution and platform-specific wheel distributions.
 
 PYTHON=python3
 
 make_wheel()
 {
-  $PYTHON setup.py clean --all
   PYTHON_SOUNDDEVICE_PLATFORM=$1 PYTHON_SOUNDDEVICE_ARCHITECTURE=$2 \
-    $PYTHON setup.py bdist_wheel
+    $PYTHON -m build
 }
-
-rm -rf sounddevice.egg-info/
-$PYTHON setup.py clean --all
-$PYTHON setup.py sdist
-
-# This creates a "pure" wheel:
-make_wheel Linux
 
 # This is always 64bit:
 make_wheel Darwin
@@ -25,4 +19,8 @@ make_wheel Windows 32bit
 
 make_wheel Windows 64bit
 
-$PYTHON setup.py clean --all
+# This makes sure that the libraries are not copied to the final sdist:
+rm -rf sounddevice.egg-info/
+
+# This creates a "pure" wheel:
+make_wheel Linux
