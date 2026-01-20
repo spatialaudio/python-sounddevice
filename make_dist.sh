a73e1bcc1781f66c7d195d52e48f73516b50a6f6
Matthias Geier <Matthias.Geier@gmail.com>
2026-01-20 20:36:23 +0100

    Use "uv build" instead of "uv run python -m build"

--- make_dist.sh
+++ make_dist.sh
@@ -4,15 +4,13 @@ set -euo pipefail
 
 # Create a source distribution and platform-specific wheel distributions.
 
-PYTHON="uv run --locked --with build python"
-
 make_wheel()
 {
   # This makes sure that only the appropriate libraries are copied:
-  rm -rf sounddevice.egg-info/
+  rm -rf build/
 
-  PYTHON_SOUNDDEVICE_PLATFORM=$1 PYTHON_SOUNDDEVICE_ARCHITECTURE=${2:-} \
-    $PYTHON -m build
+  # This builds an sdist (including libs) and uses that to build a wheel:
+  PYTHON_SOUNDDEVICE_PLATFORM=$1 PYTHON_SOUNDDEVICE_ARCHITECTURE=${2:-} uv build
 }
 
 # This is always 64bit:
@@ -26,3 +24,5 @@ make_wheel Windows arm64
 
 # This creates a "pure" wheel and an sdist without libraries:
 make_wheel Linux
+
+# NB: "Linux" must be last to get a clean sdist!
